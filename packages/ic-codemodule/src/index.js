110fcdbf47610712d92b73d87cbf007321586f8d
xiaolei <lei.xiao@ifchange.com>
2019-03-20 13:47:04 +0800

    test: prettier

--- packages/ic-codemodule/src/index.js
+++ packages/ic-codemodule/src/index.js
@@ -1,6 +1,8 @@
 export default async function delayPromise(str, ms = 0) {
-  const promise = new Promise(resolve => {
-    setTimeout(() => resolve(str), ms)
-  })
-  return await promise
+	const promise = new Promise(resolve => {
+		setTimeout(() => resolve(str), ms)
+	})
+	console.log('ss');
+	console.log("ss");
+	return await promise
 }
