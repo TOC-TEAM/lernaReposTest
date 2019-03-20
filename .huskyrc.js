87d699e048123132c16c8dd89d84e7e0dd3f39ec
xiaolei <lei.xiao@ifchange.com>
2019-03-20 14:18:51 +0800

    test: ssss

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,5 +2,6 @@ const task = arr => arr.join(' && ');
 module.exports = {
   hooks: {
     'pre-commit': task(['echo "======"', 'lint-staged', 'echo "======"']),
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
