ea5d759d3e0db40507723826b8b7adf7b6a3112a
xiaolei <lei.xiao@ifchange.com>
2019-03-20 17:47:54 +0800

    test: feature/bugfix

--- packages/ic-codemodule/src/index.js
+++ packages/ic-codemodule/src/index.js
@@ -2,9 +2,6 @@ export default async function delayPromise(str, ms = 0) {
   const promise = new Promise(resolve => {
     setTimeout(() => resolve(str), ms);
   });
-  console.log('sss');
-  console.log('sss');
-  console.log('sss');
-  console.log('sss');
+  console.log('feature/bugfix');
   return await promise;
 }
