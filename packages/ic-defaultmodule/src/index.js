2b77d3bb65dd3be82c2860194545abf76a207a63
xiaolei <lei.xiao@ifchange.com>
2019-03-21 23:35:51 +0800

    feat: lerna======

--- packages/ic-defaultmodule/src/index.js
+++ packages/ic-defaultmodule/src/index.js
@@ -1,11 +1,13 @@
 import ExampleComponent from 'ic-reactmodule';
 export default async function delayPromise(str, ms = 0) {
-  const promise = new Promise(resolve => {
-    setTimeout(() => resolve(str), ms);
-  });
-  console.log('default');
-  console.log('default');
-  console.log('default');
+	const promise = new Promise(resolve => {
+		setTimeout(() => resolve(str), ms);
+	});
+	console.log('default');
+	console.log('default');
+	console.log('default');
+	console.log('default');
+	console.log('default');
 
-  return await promise;
+	return await promise;
 }
