7c9b7806721e6b1f410356ce48c6452bc0573ba3
xiaolei <lei.xiao@ifchange.com>
2019-03-21 23:17:26 +0800

    test: aa

--- packages/ic-defaultmodule/src/index.js
+++ packages/ic-defaultmodule/src/index.js
@@ -4,6 +4,8 @@ export default async function delayPromise(str, ms = 0) {
     setTimeout(() => resolve(str), ms);
   });
   console.log('default');
+  console.log('default');
+  console.log('default');
 
   return await promise;
 }
